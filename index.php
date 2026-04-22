edd96266bea5b2195c53942d3babfc04339eaffc
MiguelEnrique1 <a20240318@utem.edu.mx>
2026-04-21 19:27:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Conexión a la BD
-$conn = new mysqli("localhost","root","","aranceles");
+$conn = new mysqli("HOST","USER","PASSWORD","DB");
 if($conn->connect_error){
   die("Error de conexión: " . $conn->connect_error);
 }
