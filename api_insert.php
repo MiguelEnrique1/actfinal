09bf409551e2aaaa537426e5018601763c32729b
MiguelEnrique1 <a20240318@utem.edu.mx>
2026-04-22 12:31:12 -0600

    Update api_insert.php

--- api_insert.php
+++ api_insert.php
@@ -1,15 +1,29 @@
 <?php
 include("conexion.php");
 
+// Leer JSON
 $data = json_decode(file_get_contents("php://input"), true);
+
+// Validar datos
+if(
+  !$data ||
+  !isset($data["tabla"]) ||
+  !isset($data["nombre"]) ||
+  !isset($data["valor"]) ||
+  !isset($data["fecha_registro"])
+){
+  echo json_encode(["error"=>"Datos incompletos"]);
+  exit;
+}
 
 $tabla = $data["tabla"];
 $nombre = $data["nombre"];
 $valor = $data["valor"];
 $fecha = $data["fecha_registro"];
 
-$sql = "INSERT INTO $tabla (nombre, valor, fecha_registro) VALUES ($1,$2,$3)";
-$res = pg_query_params($conn, $sql, [$nombre,$valor,$fecha]);
+// Query segura
+$sql = "INSERT INTO $tabla (nombre, valor, fecha_registro) VALUES ($1, $2, $3)";
+$res = pg_query_params($conn, $sql, [$nombre, $valor, $fecha]);
 
 if($res){
   echo json_encode(["mensaje"=>"Insertado correctamente"]);
